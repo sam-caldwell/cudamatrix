ec24dce737176d5b4e005cc29136bbc819def5c2
Sam Caldwell <github@samcaldwell.net>
2024-07-13 21:46:23 -0500

    current state of the kernel

--- c/kernels/add.h
+++ c/kernels/add.h
@@ -8,13 +8,26 @@
  *      Note: we do not check dimensions of the matrices for performance
  *            and we expect that the caller would have done this instead.
  */
-__global__ void matrixAddKernel(double* a, double* b, double* c, int size, int *gpuError) {
-    printf("matrixAddKernel()\n");
-    int idx = blockDim.x * blockIdx.x + threadIdx.x;
-    if (idx < size) {
-        c[idx] = a[idx] + b[idx];
-        printf("Thread %d: (idx:%d) a=%f  b=%f c=%f\n", threadIdx.x, idx, a[idx], b[idx], c[idx]);
-    }
+//__global__ void matrixAddKernel(double* a, double* b, double* c, int size, int *gpuError) {
+//    printf("matrixAddKernel()\n");
+//    int idx = blockDim.x * blockIdx.x + threadIdx.x;
+//    if (idx < size) {
+//        c[idx] = a[idx] + b[idx];
+//        printf("Thread %d: (idx:%d) a=%f  b=%f c=%f\n", threadIdx.x, idx, a[idx], b[idx], c[idx]);
+//    }
+//
+//    *gpuError=0;
+//}
+__global__ void matrixAddKernel(double *a, double *b, double *c, int rows, int cols, int *gpuError) {
+    int row = blockIdx.x * blockDim.x + threadIdx.x;
+    int column = blockIdx.y * blockDim.y + threadIdx.y;
 
-    *gpuError=0;
+    if (row < rows && column < cols) {
+        int index = row * cols + column;
+        c[index] = a[index] + b[index];
+        *gpuError=0;
+        return;
+    }
+    *gpuError=-1;
+    return;
 }
