39e3f4ca73cb38ed272b5ea1558e6d3e78f01ee4
Sam Caldwell <github@samcaldwell.net>
2024-07-13 23:22:53 -0500

    starting work to get multiply up and running

--- c/kernels/multiply.h
+++ c/kernels/multiply.h
@@ -9,15 +9,21 @@
  *            and we expect that the caller would have done this instead.
  */
 __global__ void matrixMultiplyKernel(double* a, double* b, double* c, int rows, int cols, int *gpuError) {
+    const int boundCheckError = -1;
+
     int idx = blockDim.x * blockIdx.x + threadIdx.x;
+
     int row = idx / cols;
     int col = idx % cols;
+
     if (row < rows && col < cols) {
         double value = 0.0;
         for (int k = 0; k < cols; ++k) {
             value += a[row * cols + k] * b[k * cols + col];
         }
         c[row * cols + col] = value;
+        return;
     }
-    *gpuError=0;
+
+    atomicExch(gpuError, boundCheckError);
 }
